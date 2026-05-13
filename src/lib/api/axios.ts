933211be62544ed3992e0d96f53a73a027cf2429
TanvirRahman888 <tanvirrahman8888@gmail.com>
2026-05-14 03:19:46 +0600

    first commit

---
+++ src/lib/api/axios.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api";
+
+export const api = axios.create({
+  baseURL: API_BASE_URL,
+  withCredentials: true,
+});
+
+api.interceptors.request.use((config) => {
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("shopwise_token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+
+  return config;
+});
